6120182b9a57d2c6f6f19053f43e978a1b9d59ea
oscarheimdahl <o.heimdahl@gmail.com>
2021-02-27 15:18:17 +0100

    cm

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+    @Input() scrollIndicator: string |undefined;
   mobile: boolean = false;
   shadow: string = 'box-shadow: rgba(49, 20, 209, 0.356) 0px 48px 100px 0px;';
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,11 @@ export class AppComponent {
   title = 'portfolio';
   showProfile: string = 'visible';
   showProjects: string = 'hidden';
+  scrollIndicator: string = '1';
 
   @HostListener('window:scroll', ['$event']) // for window scroll events
   onScroll(event: any) {
+    this.scrollIndicator = '0';
     if (window.screen.width < 500) return;
     const showProjects = window.scrollY >= window.innerHeight * 1.1;
 
